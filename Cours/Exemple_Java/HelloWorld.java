37af03fceede45d5c8be90d7b2b143a4eb9cb3ae
Jean-Charles <jeancharles.duffau@gmail.com>
2022-09-08 15:05:01 +0200

    Premiers ajouts

---
+++ Cours/Exemple_Java/HelloWorld.java
@@ -0,0 +1,34 @@
+
+/* ---------------coucou 
+ * 
+ * 
+ * -----------------*/
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		// String[][] tab = {
+		// 		{"lundi","mardi","mercredi"},
+		// 		{"janvier","fevrier","mars","avril"},
+		// 		{"un","deux"}
+		// };
+
+		// for(int i=0 ; i<tab.length ; i++) {
+
+		// 	for(int j=0 ; j<tab[i].length ; j++)  {
+		// 		System.out.print(tab[i][j]);
+		// 		System.out.print(" ");
+		// 	}
+		// 	System.out.println();
+		// }
+		//String firstname = "luke";
+		//System.out.println(firstname=="luke");// true , déjà en mémoire
+		//System.out.println(firstname == new String("luke"));//false
+		// optimisation de compilation
+
+		int value = 10;
+		System.out.print(++value);
+		System.out.print(--value);
+		System.out.print(value++);
+		System.out.print(value--);
+	}
+}
